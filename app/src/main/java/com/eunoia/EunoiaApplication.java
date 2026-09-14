696cc4a45ade88b6cde02ac117d2403756fb593c
Woojin CHO <littletree0330@gmail.com>
2026-09-14 18:32:18 +0900

    first commit

---
+++ app/src/main/java/com/eunoia/EunoiaApplication.java
@@ -0,0 +1,12 @@
+package com.eunoia;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EunoiaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EunoiaApplication.class, args);
+    }
+}
